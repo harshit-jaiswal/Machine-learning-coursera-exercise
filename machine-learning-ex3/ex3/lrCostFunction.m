40d8434d03ee0fba4f8a38dba2b6e500048eb2f2
Harshit Jaiswal <Hjaiswal02@gmail.com>
2020-04-30 17:15:03 +0530

    Done with ex3

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -37,7 +37,10 @@ grad = zeros(size(theta));
 %
 
 
-
+h = sigmoid(X*theta);
+theta1 = [0 ; theta(2:size(theta), :)]
+J = ((-y)'*log(h)-(1-y)'*log(1-h))/m + (lambda/(2*m))*sum(theta(2:end).^2);
+grad = (X'*(h - y)+lambda*theta1)/m;
 
 
 
